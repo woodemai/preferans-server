5b2f4093bffbd75ac29858a4c84fec060ff3d816
woodemai <n.savchenkoo73@gmail.com>
2024-02-01 18:12:04 +0300

    update SocketExceptionHandler.java

--- src/main/java/com/ru/preferans/errors/SocketExceptionHandler.java
+++ src/main/java/com/ru/preferans/errors/SocketExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ru.preferans.errors;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.listener.ExceptionListener;
+import com.corundumstudio.socketio.listener.ExceptionListenerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
@@ -13,7 +14,7 @@ import java.util.List;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 @Slf4j
-public class SocketExceptionHandler implements ExceptionListener {
+public class SocketExceptionHandler extends ExceptionListenerAdapter {
 
     @Override
     public void onEventException(Exception e, List<Object> args, SocketIOClient client) {
@@ -30,22 +31,5 @@ public class SocketExceptionHandler implements ExceptionListener {
     public void onConnectException(Exception e, SocketIOClient client) {
         log.error("Socket connect error: {}", e.getLocalizedMessage());
 
-    }
-
-    @Override
-    public void onPingException(Exception e, SocketIOClient client) {
-        log.error("Socket ping: {}", e.getLocalizedMessage());
-
-    }
-
-    @Override
-    public void onPongException(Exception e, SocketIOClient client) {
-        log.error("Socket pong error: {}", e.getLocalizedMessage());
-
-    }
-
-    @Override
-    public boolean exceptionCaught(ChannelHandlerContext ctx, Throwable e) {
-        return false;
     }
 }
